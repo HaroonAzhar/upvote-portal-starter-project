2daadc588286e84cb20bc6c9cc1f5ff9a6faa45c
Haroon Azhar Khan <haroon1azhar@gmail.com>
2022-12-16 21:40:39 +0000

    added connect wallet and account features

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,74 @@
-import React from "react";
+import React,{ useEffect, useState} from "react";
 import { ethers } from "ethers";
 import './App.css';
 
+const getEthereumObject = () => window.ethereum;
+/*
+ * This function returns the first linked account found.
+ * If there is no account linked, it will return null.
+ */
+const findMetaMaskAccount = async () => {
+  try {
+    const ethereum = getEthereumObject();
+
+    /*
+    * First make sure we have access to the Ethereum object.
+    */
+    if (!ethereum) {
+      console.error("Make sure you have Metamask!");
+      return null;
+    }
+
+    console.log("We have the Ethereum object", ethereum);
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      return account;
+    } else {
+      console.error("No authorized account found");
+      return null;
+    }
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
 export default function App() {
+  
+   const [currentAccount, setCurrentAccount] = useState("");
+  const connectWallet = async () => {
+    try {
+      const ethereum = getEthereumObject();
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
+  /*
+   * The passed callback function will be run when the page loads.
+   * More technically, when the App component "mounts".
+   */
+  useEffect(() => {
+    findMetaMaskAccount().then((account) => {
+      if (account !== null) {
+        setCurrentAccount(account);
+      }
+    });
+  }, []);
 
   const wave = () => {
     
@@ -23,6 +89,11 @@ export default function App() {
         <button className="waveButton" onClick={wave}>
           Wave at Me
         </button>
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
       </div>
     </div>
   );
